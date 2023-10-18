5ddb15b82306553e2aaf5a3f355fed97f6405dfc
Nicholas Minor <nrminor@wisc.edu>
2023-10-18 11:25:37 -0500

    debugging complete for now

--- src/ALPINEExplorer.jl
+++ src/ALPINEExplorer.jl
@@ -52,11 +52,11 @@ than two levels of results directories to traverse.
 struct SearchBranch
     parent_dir::String
     geography::String
-    double::Option{String}
-    anachron::Option{String}
-    highdist::Option{String}
-    early_stats::Option{String}
-    late_stats::Option{String}
+    double::Union{String, Missing}
+    anachron::Union{String, Missing}
+    highdist::Union{String, Missing}
+    early_stats::Union{String, Missing}
+    late_stats::Union{String, Missing}
 end
 
 """
@@ -78,18 +78,56 @@ Returns:
  - `Dict{Symbol, SearchBranch}`: A dictionary specifying the search tree, where each
  geography has its own associated `SearchBranch` of files and directories to search.
 """
-function define_search_tree(path_dict::Dict{String, String}, root_dir::String)::Dict{Symbol, SearchBranch}
+function define_search_tree(path_dict::Dict{String, String})::Dict{Symbol, SearchBranch}
 
     search_tree = Dict{Symbol,SearchBranch}()
+    search_tree = Dict{Symbol, SearchBranch}()
     for (geo, path) in path_dict
+
+        # check for paths
+        double =
+            if length(readdir(glob"*double_candidates", path)) >
+                0
+                readdir(glob"*double_candidates", path)[1]
+            else
+                missing
+            end
+        anachron =
+            if length(readdir(glob"*metadata_candidates", path)) >
+            0
+                readdir(glob"*metadata_candidates", path)[1]
+            else
+                missing
+            end
+        highdist =
+            if length(readdir(glob"*high_distance_clusters", path)) >
+            0
+                readdir(glob"*high_distance_clusters", path)[1]
+            else
+                missing
+            end
+        early_stats =
+            if length(readdir(glob"*early_stats.tsv", path)) >
+            0
+                readdir(glob"*early_stats.tsv", path)[1]
+            else
+                missing
+            end
+        late_stats =
+            if length(readdir(glob"*late_stats.tsv", path)) >
+            0
+                readdir(glob"*late_stats.tsv", path)[1]
+            else
+                missing
+            end
         search_tree[Symbol(geo)] = SearchBranch(
             path,
             geo,
-            some(readdir(glob"**/double_candidates", root_dir)[1]),
-            some(readdir(glob"**/metadata_candidates", root_dir)[1]),
-            some(readdir(glob"**/high_distance_clusters", root_dir)[1]),
-            some(readdir(glob"**/early_stats.tsv", root_dir)[1]),
-            some(readdir(glob"**/late_stats.tsv", root_dir)[1])
+            double,
+            anachron,
+            highdist,
+            early_stats,
+            late_stats,
         )
     end
 
@@ -103,14 +141,13 @@ the pipeline meant to describe the number of input sequences. `get_early_count`
 does so for each geography independently so that a dataframe of statistics
 can be generated in a vectorized manner.
 """
-function get_early_count(path::Option{String})
+function get_early_count(path::Union{String, Missing})
 
-    stats_path = @? path
-    if stats_path === nothing
-        return nothing
+    if path === missing
+        return missing
     end
 
-    stats_df = CSV.read(stats_path, delim='\t', DataFrame)
+    stats_df = CSV.read(path, delim='\t', DataFrame)
 
     count = stats_df.num_seqs[1]
 
@@ -125,14 +162,13 @@ which is to say sequences from viral lineages that are both highly evolved
 and anachronistic. `get_late_count` does so for each geography independently
 so that a dataframe of statistics can be generated in a vectorized manner.
 """
-function get_late_count(path::Option{String})
+function get_late_count(path::Union{String, Missing})
 
-    stats_path = @? path
-    if stats_path === nothing
-        return nothing
+    if path === missing
+        return missing
     end
 
-    stats_df = CSV.read(stats_path, delim='\t', DataFrame)
+    stats_df = CSV.read(path, delim='\t', DataFrame)
 
     count = stats_df.num_seqs[1]
 
@@ -145,14 +181,13 @@ The function `summarize_anachrons` finds the metadata for anachronistic
 sequences and counts the number of entries, if any, for the provided
 geography and associated filepath.
 """
-function summarize_anachrons(path::Option{String})
+function summarize_anachrons(path::Union{String, Missing})
 
-    anachron_path = @? path
-    if anachron_path === nothing
-        return nothing
+    if path === missing
+        return missing
     end
 
-    anachron_df = CSV.read("$anachron_path/anachronistic_metadata_only_candidates.tsv",
+    anachron_df = CSV.read("$path/anachronistic_metadata_only_candidates.tsv",
                             delim='\t', DataFrame)
 
     return nrow(anachron_df)
@@ -164,14 +199,13 @@ The function `summarize_highdist` finds the metadata for high distance
 sequences and counts the number of entries, if any, for the provided
 geography and associated filepath.
 """
-function summarize_highdist(path::Option{String})
+function summarize_highdist(path::Union{String, Missing})
 
-    highdist_path = @? path
-    if highdist_path === nothing
-        return nothing
+    if path === missing
+        return missing
     end
 
-    high_dist_df = CSV.read("$highdist_path/high_distance_candidates.tsv",
+    high_dist_df = CSV.read("$path/high_distance_candidates.tsv",
                             delim='\t', DataFrame)
 
     return nrow(high_dist_df)
@@ -197,11 +231,13 @@ function stats_pipeline(search_tree::Dict{Symbol, SearchBranch})::Result{DataFra
         
         # generate a percentage of the total inputs that were flagged as double candidates
         transform(_, [:("Double Candidate Count"), :("Input Sequence Count")] =>
-            ByRow((x, y) -> (x / y) * 100) => :("Double Candidate Prevalence (%)")) |>
+            ByRow((x, y) -> ifelse(ismissing(x) || ismissing(y), missing, (x / y) * 100)) => 
+            :("Double Candidate Prevalence (%)")) |>
         
         # Compute the rate of double candidates in a 1 in X format
         transform(_, :"Double Candidate Prevalence (%)" => 
-            ByRow(x -> ifelse(x == 0, "None", "1 in $(floor(1 / (x / 100)))")) => :"Double Candidate Rate") |>
+            ByRow(x -> ifelse(ismissing(x) || x == 0, "None", "1 in $(floor(1 / (x / 100)))")) => 
+            :"Double Candidate Rate") |>
 
         # count the number of anachronistic lineages
         hcat(_, DataFrame("Anachronistic Count" =>
@@ -209,11 +245,13 @@ function stats_pipeline(search_tree::Dict{Symbol, SearchBranch})::Result{DataFra
 
         # generate a percentage of the total inputs that were flagged as anachronistics
         transform(_, [:("Anachronistic Count"), :("Input Sequence Count")] =>
-            ByRow((x, y) -> (x / y) * 100) => :("Anachronistic Prevalence (%)")) |>
+            ByRow((x, y) -> ifelse(ismissing(x) || ismissing(y), missing, (x / y) * 100)) => 
+            :("Anachronistic Prevalence (%)")) |>
         
         # Compute the rate of anachronistic candidates in a 1 in X format
         transform(_, :"Anachronistic Prevalence (%)" => 
-            ByRow(x -> ifelse(x == 0, "None", "1 in $(floor(1 / (x / 100)))")) => :"Anachronistic Rate") |>
+            ByRow(x -> ifelse(ismissing(x) || x == 0, "None", "1 in $(floor(1 / (x / 100)))")) => 
+            :"Anachronistic Rate") |>
         
         # get the number of high distance candidates
         hcat(_, DataFrame("High Distance Count" =>
@@ -225,7 +263,8 @@ function stats_pipeline(search_tree::Dict{Symbol, SearchBranch})::Result{DataFra
         
         # Compute the rate of high distance candidates in a 1 in X format
         transform(_, :"High Distance Prevalence (%)" => 
-            ByRow(x -> ifelse(x == 0, "None", "1 in $(floor(1 / (x / 100)))")) => :"High Distance Rate")
+            ByRow(x -> ifelse(ismissing(x) || x == 0, "None", "1 in $(floor(1 / (x / 100)))")) => 
+            :"High Distance Rate")
         
     # save as an excel file
     save("alpine_run_statistics.xlsx", stats_df)
